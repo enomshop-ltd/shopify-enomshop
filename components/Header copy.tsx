80cd39e441f1f903dc6a46a0d3486c94af26e556
Nelly #1 <122103189+enomshop-ltd@users.noreply.github.com>
2025-09-19 17:43:09 +0300

    gh

--- components/Header copy.tsx
+++ components/Header copy.tsx
@@ -2,7 +2,13 @@ import Cart from "../islands/Cart.tsx";
 
 export function Header() {
   return (
-    <header class="h-[80px] sm:!h-[100px] w-full relative">
+    <header
+      class="h-[110px] sm:!h-[144px] w-full bg-cover bg-no-repeat relative"
+      style={{
+        backgroundImage: "url(/header_bg.svg)",
+      }}
+    >
+      <div class="rainfall w-full h-full absolute" />
       <nav class="w-11/12 h-24 max-w-5xl mx-auto flex items-center justify-between relative">
         <a href="/">
           <img
